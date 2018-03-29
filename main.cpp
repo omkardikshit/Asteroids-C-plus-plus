38032676cac6aa06338c522e3e60f39735445095
Omkar Dixit <red.destroy27@gmail.com>
2018-03-29 09:13:31 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,49 +11,6 @@ extern int height;
 extern int fps;
 extern Vector2f pos;
 extern RenderWindow window;
-/*class Ship{
-public:
-	Vector2f pos = Vector2f(width / 2, height / 2);
-	int r = 20;
-	int x  = 0;
-	Vector2f vel = Vector2f(0, 0);
-	void render() {
-		ConvexShape convex;
-		convex.setPointCount(3);
-		convex.setPoint(0, Vector2f(-r, r));
-		convex.setPoint(1, Vector2f(r, r));
-		convex.setPoint(2, Vector2f(0, -r));
-		convex.setPosition(pos);
-		convex.setFillColor(Color(51, 51, 51));
-		convex.setOutlineColor(Color(255, 255, 255));
-		convex.setOutlineThickness(2);
-		convex.setRotation(heading+90);
-		window.draw(convex);
-	}
-
-	void update() {
-		pos += vel;
-		vel.x *= 0.97;
-		vel.y *= 0.97;
-	}
-
-	void boost() {
-		Vector2f force = Vector2f(cos(heading*dtr)*0.2,sin(heading*dtr)*0.2);
-		vel.x += force.x;
-		vel.y += force.y;
-	}
-
-	void stop(){
-		Vector2f force = Vector2f(0,0);
-		vel.x *= force.x;
-		vel.y *= force.y;
-	}
-
-	void turn(int angle) {
-		heading += angle;
-	}
-
-};*/
 
 int main() {
 	window.setFramerateLimit(fps);
